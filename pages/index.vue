ac138dcd489604f2f1438a355a3a4bbc6c55ad2b
Xuffasch <tristan.lucas@gmail.com>
2021-03-22 09:34:57 +0100

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <h1>Hello World!</h1>
+    <NuxtLink to='/fun'>Fun page</NuxtLink>
+  </main>
+</template>
+
+<style scoped>
+  a {
+    display: block;
+    padding: 1em;
+    margin: 1em;
+    background-color: yellow;
+    font-size: large;
+  }
+
+
+</style>
